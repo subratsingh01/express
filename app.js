ac401434a0898a7d1dded17fd0cb852d14d5604b
subrat singh <subratprivate777@gmail.com>
2024-10-27 10:25:22 +0530

    routs added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("ha batao kya hua slash me ho tum => paglu home");
+})
+
 app.get("/hi", (req, res) => {
-    res.send("ha batao kya hua");
+    res.send("ha batao kya hua hi bo rhe ho jo");
+})
+
+app.get("/bye", (req, res) => {
+    res.send("ha batao kya hua bye mt bolo");
+})
+
+app.get("/subrat", (req, res) => {
+    res.send("ha batao kya hua subrat");
 })
 
 app.listen(3000, () => console.log("listening on port 3000..."))
